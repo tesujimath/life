15044eb87c30d1eed5e64b16d6e2a43bb11d1163
Simon Guest <simon.guest@tesujimath.org>
2023-01-09 18:06:05 +1300

    Insertion of span into row, some tests (failing)

--- src/playfield.rs
+++ src/playfield.rs
@@ -1,3 +1,6 @@
+// TODO remove suppression for dead code warning
+#![allow(dead_code)]
+
 use std::cmp::Ordering;
 use std::collections::VecDeque;
 
@@ -8,36 +11,54 @@ trait Ord2<RHS> {
 }
 
 /// a horizontal block of contiguous cells
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct Span {
     /// position of leftmost cell, in cell units
     left: i32,
     cells: VecDeque<u8>,
 }
 
-impl PartialEq<i32> for Span {
-    fn eq(&self, x: &i32) -> bool {
-        *x >= self.left && *x < self.left + self.cells.len() as i32
-    }
-}
-
-impl PartialOrd<i32> for Span {
-    fn partial_cmp(&self, x: &i32) -> Option<Ordering> {
-        if *x < self.left {
-            Some(Ordering::Greater)
-        } else if *x < self.left + self.cells.len() as i32 {
-            Some(Ordering::Equal)
-        } else {
-            Some(Ordering::Less)
+impl Span {
+    fn new(i_cell: i32, cell: u8) -> Span {
+        Span {
+            left: i_cell,
+            cells: VecDeque::from(vec![cell]),
         }
+    }
+
+    fn contains(&self, i_cell: i32) -> bool {
+        i_cell >= self.left && i_cell < self.left + self.cells.len() as i32
+    }
+
+    fn adjoins_left(&self, i_cell: i32) -> bool {
+        i_cell == self.left - 1
+    }
+
+    fn adjoins_right(&self, i_cell: i32) -> bool {
+        i_cell == self.left + self.cells.len() as i32
+    }
+
+    // TODO implement via Index and IndexMut traits
+    fn set(&mut self, i_cell: i32, cell: u8) {
+        assert!(self.contains(i_cell));
+        self.cells[(i_cell - self.left) as usize] = cell;
+    }
+
+    fn push_front(&mut self, cell: u8) {
+        self.cells.push_front(cell);
+        self.left -= 1;
+    }
+
+    fn push_back(&mut self, cell: u8) {
+        self.cells.push_back(cell);
     }
 }
 
 impl Ord2<i32> for Span {
-    fn cmp(&self, x: &i32) -> Ordering {
-        if *x < self.left {
+    fn cmp(&self, i_cell: &i32) -> Ordering {
+        if *i_cell < self.left {
             Ordering::Greater
-        } else if *x < self.left + self.cells.len() as i32 {
+        } else if *i_cell < self.left + self.cells.len() as i32 {
             Ordering::Equal
         } else {
             Ordering::Less
@@ -46,19 +67,52 @@ impl Ord2<i32> for Span {
 }
 
 /// an ordered list of spans, ordered by `left`, and coelesced opportunistically
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct Row {
     spans: VecDeque<Span>,
 }
 
 impl Row {
-    fn set(&mut self, x: i32, val: u8) {
-        match self
-            .spans
-            .binary_search_by(|span| Ordering::Greater /* span.partial_cmp(&x)*/)
-        {
-            Ok(i) => (),
-            Err(i) => (),
+    fn new() -> Row {
+        Row {
+            spans: VecDeque::new(),
+        }
+    }
+
+    fn set(&mut self, i_cell: i32, cell: u8) {
+        match self.spans.binary_search_by(|span| span.cmp(&i_cell)) {
+            Ok(i_span) => {
+                self.spans[i_span].set(i_cell, cell);
+            }
+            Err(i_span) => {
+                if i_span < self.spans.len() {
+                    let span_i = &mut self.spans[i_span];
+                    if span_i.adjoins_left(i_cell) {
+                        span_i.push_front(cell);
+                    } else if i_span > 0 {
+                        let span_left = &mut self.spans[i_span - 1];
+                        if span_left.adjoins_right(i_cell) {
+                            span_left.push_back(cell);
+                        } else {
+                            self.spans.insert(i_span, Span::new(i_cell, cell));
+                        }
+                    } else {
+                        self.spans.push_back(Span::new(i_cell, cell));
+                    }
+                } else {
+                    match self.spans.back_mut() {
+                        Some(span_left) => {
+                            // TODO extract out common code with above
+                            if span_left.adjoins_right(i_cell) {
+                                span_left.push_back(cell);
+                            } else {
+                                self.spans.insert(i_span, Span::new(i_cell, cell));
+                            }
+                        }
+                        None => self.spans.push_back(Span::new(i_cell, cell)),
+                    }
+                }
+            }
         }
     }
 }
--- src/playfield/tests.rs
+++ src/playfield/tests.rs
@@ -66,3 +66,78 @@ fn test_span_ord2() {
         Ordering::Equal
     );
 }
+
+#[test]
+fn test_spans_binary_search() {
+    let spans = VecDeque::from(vec![
+        Span {
+            left: 0,
+            cells: VecDeque::from(vec![5u8, 7u8]),
+        },
+        Span {
+            left: 3,
+            cells: VecDeque::from(vec![3u8]),
+        },
+    ]);
+
+    assert_eq!(spans.binary_search_by(|span| span.cmp(&-1)), Err(0));
+    assert_eq!(spans.binary_search_by(|span| span.cmp(&1)), Ok(0));
+    assert_eq!(spans.binary_search_by(|span| span.cmp(&2)), Err(1));
+    assert_eq!(spans.binary_search_by(|span| span.cmp(&3)), Ok(1));
+    assert_eq!(spans.binary_search_by(|span| span.cmp(&4)), Err(2));
+}
+
+#[test]
+fn test_row_set() {
+    let row = &mut Row::new();
+
+    row.set(10, 10u8);
+    assert_eq!(
+        *row,
+        Row {
+            spans: VecDeque::from(vec![Span {
+                left: 10,
+                cells: VecDeque::from(vec![10u8])
+            },])
+        }
+    );
+
+    row.set(11, 11u8);
+    assert_eq!(
+        *row,
+        Row {
+            spans: VecDeque::from(vec![Span {
+                left: 10,
+                cells: VecDeque::from(vec![10u8, 11u8])
+            },])
+        }
+    );
+
+    row.set(9, 9u8);
+    assert_eq!(
+        *row,
+        Row {
+            spans: VecDeque::from(vec![Span {
+                left: 9,
+                cells: VecDeque::from(vec![9u8, 10u8, 11u8])
+            }])
+        }
+    );
+
+    row.set(7, 7u8);
+    assert_eq!(
+        *row,
+        Row {
+            spans: VecDeque::from(vec![
+                Span {
+                    left: 7,
+                    cells: VecDeque::from(vec![7u8])
+                },
+                Span {
+                    left: 9,
+                    cells: VecDeque::from(vec![9u8, 10u8, 11u8])
+                }
+            ])
+        }
+    );
+}
