5b86bccc9eae6239bc8a96f5541dca7421507682
Simon Guest <simon.guest@tesujimath.org>
2023-01-11 12:09:45 +1300

    IndexMut trait for Contig

--- src/playfield.rs
+++ src/playfield.rs
@@ -8,6 +8,7 @@ use std::cmp::Ordering;
 use std::collections::VecDeque;
 use std::ops::Add;
 use std::ops::Index;
+use std::ops::IndexMut;
 use std::ops::Sub;
 use std::ops::SubAssign;
 
@@ -48,22 +49,12 @@ where
         }
     }
 
-    fn is_in_bounds(&self, i: Idx) -> bool {
-        i >= self.origin && i < self.origin + Idx::from_usize(self.items.len()).unwrap()
-    }
-
     fn adjoins_left(&self, i: Idx) -> bool {
         i == self.origin - Idx::one()
     }
 
     fn adjoins_right(&self, i: Idx) -> bool {
         i == self.origin + Idx::from_usize(self.items.len()).unwrap()
-    }
-
-    // TODO implement via Index and IndexMut traits
-    fn set(&mut self, i: Idx, item: T) {
-        assert!(self.is_in_bounds(i));
-        self.items[Idx::to_usize(&(i - self.origin)).unwrap()] = item;
     }
 
     fn push_front(&mut self, item: T) {
@@ -95,8 +86,24 @@ where
     type Output = T;
 
     fn index(&self, i: Idx) -> &Self::Output {
-        assert!(self.is_in_bounds(i));
         &self.items[Idx::to_usize(&(i - self.origin)).unwrap()]
+    }
+}
+
+impl<Idx, T> IndexMut<Idx> for Contig<Idx, T>
+where
+    Idx: Copy
+        + One
+        + FromPrimitive
+        + ToPrimitive
+        + Add<Output = Idx>
+        + Sub<Output = Idx>
+        + PartialOrd
+        + SubAssign,
+    T: Copy,
+{
+    fn index_mut(&mut self, i: Idx) -> &mut Self::Output {
+        &mut self.items[Idx::to_usize(&(i - self.origin)).unwrap()]
     }
 }
 
@@ -147,7 +154,7 @@ where
     fn set(&mut self, i: Idx, item: T) {
         match self.contigs.binary_search_by(|c| c.cmp(&i)) {
             Ok(i_c) => {
-                self.contigs[i_c].set(i, item);
+                self.contigs[i_c][i] = item;
             }
             Err(i_c) => {
                 if i_c < self.contigs.len() {
