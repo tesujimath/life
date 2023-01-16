e7a725fd86ed3e020d7e207bd346c17652931840
Simon Guest <simon.guest@tesujimath.org>
2023-01-16 17:08:07 +1300

    PairwiseOrZeroes iterator

--- src/playfield.rs
+++ src/playfield.rs
@@ -1,9 +1,9 @@
 // TODO remove suppression for dead code warning
-#![allow(dead_code)]
+#![allow(dead_code, unused_variables)]
 
 use super::contig::CartesianContigs;
-use std::iter::repeat;
 use std::iter::Iterator;
+use std::slice::Iter;
 
 /// Array of bits, organised as double rows.
 /// Each item represents two rows of bits of width `BLOCKSIZE`.
@@ -18,6 +18,36 @@ struct Playfield {
 const BLOCKSIZE: usize = 4;
 const ZEROBLOCK: u8 = 0;
 
+struct PairwiseOrZeroes<'a> {
+    i1: Iter<'a, u8>,
+    i2o: Option<Iter<'a, u8>>,
+}
+
+impl<'a> PairwiseOrZeroes<'a> {
+    fn new(rows: &'a [&'a [u8]]) -> PairwiseOrZeroes<'a> {
+        PairwiseOrZeroes {
+            i1: rows[0].iter(),
+            i2o: rows.get(1).map(|r| r.iter()),
+        }
+    }
+}
+
+impl<'a> Iterator for PairwiseOrZeroes<'a> {
+    type Item = (&'a u8, &'a u8);
+
+    fn next(&mut self) -> Option<(&'a u8, &'a u8)> {
+        match &mut self.i2o {
+            Some(i2) => match (self.i1.next(), i2.next()) {
+                (Some(x1), Some(x2)) => Some((x1, x2)),
+                (Some(x1), None) => Some((x1, &0u8)),
+                (None, Some(x2)) => Some((&0u8, x2)),
+                (None, None) => None,
+            },
+            None => self.i1.next().map(|x1| (x1, &0u8)),
+        }
+    }
+}
+
 impl Playfield {
     fn new() -> Playfield {
         Playfield {
@@ -26,19 +56,14 @@ impl Playfield {
     }
 
     fn from<I>(rows_of_bytes: &[&[u8]]) -> Playfield {
-        for double_row in rows_of_bytes.chunks(2) {
-            let r0 = double_row[0usize].iter();
-            let zeros = &repeat(ZEROBLOCK) as &dyn Iterator<Item = u8>;
-            let r1 = double_row
-                .get(1)
-                .copied()
-                .map(|r| r.iter() as &dyn Iterator<Item = u8>)
-                .unwrap_or(zeros);
-            //for block_pair in r0.zip(r1).collect::<Vec<(&u8, &u8)>>().chunks(BLOCKSIZE) {
-            //    println!("{:?}", block_pair)
-            //}
+        for chunk in rows_of_bytes.chunks(2) {
+            for p in PairwiseOrZeroes::new(chunk) {
+                println!("pair {:?} {:?}", p.0, p.1)
+            }
         }
 
         Playfield::new()
     }
 }
+
+mod tests;
