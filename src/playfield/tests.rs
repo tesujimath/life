90264125acbc709ba52281255f509f6d91ebd879
Simon Guest <simon.guest@tesujimath.org>
2023-01-18 11:26:53 +1300

    Gave up with slices, pushing the type system too hard.

--- src/playfield/tests.rs
+++ src/playfield/tests.rs
@@ -25,31 +25,6 @@ fn test_pairwise_or_default() {
         p1.collect::<Vec<(u8, u8)>>(),
         vec![(1u8, 0u8), (2u8, 0u8), (3u8, 0u8)]
     );
-
-    // getting it working with slices of slices
-    let row1: Vec<u8> = vec![1, 2, 3];
-    let row2: Vec<u8> = vec![11, 12];
-    let slice_of_rows = &vec![&row1[..], &row2[..]][..];
-
-    let slice_of_u8 = &vec![1u8, 2, 3][..];
-    let slice_of_u8_it = slice_of_u8.into_iter();
-    for u in slice_of_u8_it {
-        assert!(u >= &0);
-    }
-
-    let slice_of_rows_it = slice_of_rows.into_iter();
-    for row in slice_of_rows_it {
-        let row_deref = *row;
-        let r_it = row_deref.into_iter();
-        for item in r_it {
-            assert!(item >= &0);
-        }
-    }
-    // let s_ref = PairwiseOrDefault::<IntoIter<&u8>, &u8>::from(s, &0);
-    // assert_eq!(
-    //     vec_ref.collect::<Vec<(&u8, &u8)>>(),
-    //     vec![(&1u8, &11u8), (&2u8, &0u8), (&3u8, &0u8)]
-    // );
 }
 
 // #[test]
