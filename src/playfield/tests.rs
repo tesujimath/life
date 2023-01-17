49b09b860fa3269de6ab70228fda3c4825ee3b0f
Simon Guest <simon.guest@tesujimath.org>
2023-01-18 11:32:13 +1300

    Chunks test

--- src/playfield/tests.rs
+++ src/playfield/tests.rs
@@ -27,29 +27,46 @@ fn test_pairwise_or_default() {
     );
 }
 
-// #[test]
-// fn test_bits_to_bytes() {
-//     use std::vec::IntoIter;
+#[test]
+fn test_chunks() {
+    use std::vec::IntoIter;
 
-//     let rows: Vec<Vec<u8>> = vec![
-//         vec![0b00111011, 0b11110001],
-//         vec![0b10101010, 0b10001001],
-//         vec![0b00001000, 0b10000001],
-//     ];
+    let rows: Vec<Vec<u8>> = vec![vec![1, 2], vec![3, 4], vec![5, 6]];
 
-//     let mut double_row = rows.chunks(2);
+    let mut double_row = rows.chunks(2);
 
-//     let r0 = double_row.next().unwrap();
-//     let p = PairwiseOrDefault::<IntoIter<u8>>::from(&r0);
+    let r0 = double_row.next().unwrap();
+    let p0 = PairwiseOrDefault::<IntoIter<u8>>::from(r0);
+    assert_eq!(p0.collect::<Vec<(u8, u8)>>(), vec![(1, 3), (2, 4),]);
 
-//     // assert_eq!(
-//     //     p.collect::<Vec<(u8, u8)>>(),
-//     //     vec![(0b00111011, 0b10101010), (0b11110001, 0b10001001),]
-//     // );
+    let r1 = double_row.next().unwrap();
+    let p1 = PairwiseOrDefault::<IntoIter<u8>>::from(r1);
+    assert_eq!(p1.collect::<Vec<(u8, u8)>>(), vec![(5, 0), (6, 0)]);
+}
 
-//     // let p = PairwiseOrDefault::<IntoIter<u8>>::from(double_row.next());
-//     // assert_eq!(
-//     //     p.collect::<Vec<(u8, u8)>>(),
-//     //     vec![(0b00001000, 0), (0b10000001, 0)]
-//     // );
-// }
+#[test]
+fn test_bits_to_bytes() {
+    use std::vec::IntoIter;
+
+    let rows: Vec<Vec<u8>> = vec![
+        vec![0b00111011, 0b11110001],
+        vec![0b10101010, 0b10001001],
+        vec![0b00001000, 0b10000001],
+    ];
+
+    let mut double_row = rows.chunks(2);
+
+    let r0 = double_row.next().unwrap();
+    let p0 = PairwiseOrDefault::<IntoIter<u8>>::from(r0);
+    assert_eq!(
+        p0.collect::<Vec<(u8, u8)>>(),
+        vec![(0b00111011, 0b10101010), (0b11110001, 0b10001001),]
+    );
+
+    let r1 = double_row.next().unwrap();
+    let p1 = PairwiseOrDefault::<IntoIter<u8>>::from(r1);
+    assert_eq!(
+        p1.collect::<Vec<(u8, u8)>>(),
+        vec![(0b00001000, 0), (0b10000001, 0)]
+    );
+}
