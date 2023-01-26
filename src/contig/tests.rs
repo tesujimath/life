71a2b361180780bd990bad1defcff9eade6aa048
Simon Guest <simon.guest@tesujimath.org>
2023-01-27 09:25:40 +1300

    Extend tests for CartesianNeighbourhoodEnumerator
    Neighbouring rows not yet included

--- src/contig/tests.rs
+++ src/contig/tests.rs
@@ -325,17 +325,72 @@ fn test_cartesian_contig_neighbourhood_enumerator() {
     cc.set(1, 0, 1u8);
     cc.set(2, 0, 2u8);
     cc.set(1, 1, 11u8);
+    cc.set(2, 3, 22u8);
 
     assert_eq!(
         enumerator_as_vec(&cc),
         vec![
+            CartesianNeighbourhood {
+                i_row: -1,
+                i_col: -1,
+                items: [
+                    [None, None, None],
+                    [None, None, None],
+                    [None, None, Some(&0)],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: -1,
+                i_col: 0,
+                items: [
+                    [None, None, None],
+                    [None, None, None],
+                    [None, Some(&0), Some(&1)],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: -1,
+                i_col: 1,
+                items: [
+                    [None, None, None],
+                    [None, None, None],
+                    [Some(&0), Some(&1), Some(&2)],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: -1,
+                i_col: 2,
+                items: [
+                    [None, None, None],
+                    [None, None, None],
+                    [Some(&1), Some(&2), None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: -1,
+                i_col: 3,
+                items: [
+                    [None, None, None],
+                    [None, None, None],
+                    [Some(&2), None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 0,
+                i_col: -1,
+                items: [
+                    [None, None, None],
+                    [None, None, Some(&0)],
+                    [None, None, None],
+                ]
+            },
             CartesianNeighbourhood {
                 i_row: 0,
                 i_col: 0,
                 items: [
                     [None, None, None],
                     [None, Some(&0), Some(&1)],
-                    [None, None, Some(&11)]
+                    [None, None, Some(&11)],
                 ]
             },
             CartesianNeighbourhood {
@@ -344,7 +399,7 @@ fn test_cartesian_contig_neighbourhood_enumerator() {
                 items: [
                     [None, None, None],
                     [Some(&0), Some(&1), Some(&2)],
-                    [None, Some(&11), None]
+                    [None, Some(&11), None],
                 ]
             },
             CartesianNeighbourhood {
@@ -353,7 +408,25 @@ fn test_cartesian_contig_neighbourhood_enumerator() {
                 items: [
                     [None, None, None],
                     [Some(&1), Some(&2), None],
-                    [Some(&11), None, None]
+                    [Some(&11), None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 0,
+                i_col: 3,
+                items: [
+                    [None, None, None],
+                    [Some(&2), None, None],
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 1,
+                i_col: 0,
+                items: [
+                    [None, Some(&0), Some(&1)],
+                    [None, None, Some(&11)],
+                    [None, None, None],
                 ]
             },
             CartesianNeighbourhood {
@@ -362,7 +435,106 @@ fn test_cartesian_contig_neighbourhood_enumerator() {
                 items: [
                     [Some(&0), Some(&1), Some(&2)],
                     [None, Some(&11), None],
-                    [None, None, None]
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 1,
+                i_col: 2,
+                items: [
+                    [Some(&1), Some(&2), None],
+                    [Some(&11), None, None],
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 2,
+                i_col: 0,
+                items: [
+                    [None, None, Some(&11)],
+                    [None, None, None],
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 2,
+                i_col: 1,
+                items: [
+                    [None, Some(&11), None],
+                    [None, None, None],
+                    [None, None, Some(&22)],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 2,
+                i_col: 2,
+                items: [
+                    [Some(&11), None, None],
+                    [None, None, None],
+                    [None, Some(&22), None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 2,
+                i_col: 3,
+                items: [
+                    [None, None, None],
+                    [None, None, None],
+                    [Some(&22), None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 3,
+                i_col: 1,
+                items: [
+                    [None, None, None],
+                    [None, None, Some(&22)],
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 3,
+                i_col: 2,
+                items: [
+                    [None, None, None],
+                    [None, Some(&22), None],
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 3,
+                i_col: 3,
+                items: [
+                    [None, None, None],
+                    [Some(&22), None, None],
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 4,
+                i_col: 1,
+                items: [
+                    [None, None, Some(&22)],
+                    [None, None, None],
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 4,
+                i_col: 2,
+                items: [
+                    [None, Some(&22), None],
+                    [None, None, None],
+                    [None, None, None],
+                ]
+            },
+            CartesianNeighbourhood {
+                i_row: 4,
+                i_col: 3,
+                items: [
+                    [Some(&22), None, None],
+                    [None, None, None],
+                    [None, None, None],
                 ]
             },
         ]
